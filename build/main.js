36a6eec9cec01f8470cfd79b2c883e5742fa5904
Fitonho <fitonho@gmail.com>
2020-06-29 04:39:49 -0300

    pequena otimização no eventListener

--- build/main.js
+++ build/main.js
@@ -4,7 +4,7 @@ let tela = new Tela(level3);
 tela.paint();
 document.addEventListener('keypress', (event) => {
     console.log(event.key + " pressed");
-    'wsda'.split('').forEach((key, index) => {
+    ['w', 's', 'd', 'a'].forEach((key, index) => {
         if (key == event.key) {
             tela.move(index);
         }
