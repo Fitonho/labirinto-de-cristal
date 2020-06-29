9a1fa0ac74b39a7030abc7cbab3bf1e1ae9a5ac5
Fitonho <fitonho@gmail.com>
2020-06-29 04:36:00 -0300

    mudanças na estrutura de diretórios

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { Tela } from './tela.js';
-import { level3 } from './levels.js';
+import { side } from './block.js'
+import { Tela } from './tela.js'
+import { level3 } from './levels.js'
+
 let tela = new Tela(level3);
 tela.paint();
+
 document.addEventListener('keypress', (event) => {
     console.log(event.key + " pressed");
     'wsda'.split('').forEach((key, index) => {
         if (key == event.key) {
-            tela.move(index);
+            tela.move(<side>index);
         }
-    });
+    })
 });
-//# sourceMappingURL=main.js.map
