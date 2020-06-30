84d5a957c1c3107721b25accc72db9be935daed7
arathi guedes <a194330@dac.unicamp.br>
2020-06-30 01:39:00 -0300

    lv4 criado

--- build/levels.js
+++ build/levels.js
@@ -18,3 +18,43 @@ export function level3(matriz) {
     return [8, 2];
 }
 //# sourceMappingURL=levels.js.map
+
+ export function level4(matriz) {
+	 let rowSize = 9;
+	 let colSize = 9;
+	 for (let i = 0; i < rowSize, i++){
+		matriz[i]=[];
+		for (let j = 0; j < colSize; j++){
+			matriz[i][j] = new Block(colors.white);
+		}
+	 }
+	 let list = [0,1,7];
+	 for (let i of list){
+		 matriz[i][0] = new Block(colors.black);
+		 matriz[i][8] = new Block(colors.black);
+	 }
+	 let list = [0,1]
+	 for (let i of list){
+		 matriz[i][1] = new Block(colors.black);
+		 matriz[i][7] = new Block(colors.black);
+	 }
+	 let list = [1, 3, 7, 8];
+	 for (let i of list){
+		 matriz[i][2] = new Block(colors.black);
+		 matriz[i][6] = new Block(colors.black);
+	 }
+	 let list = [0,3,4,7,8];
+	 for ( let i of list) {
+		 matriz[i][3] = new Block(colors.black);
+		 matriz[i][5] = new Block(colors.black);
+	 }
+	 let list = [0,3,4];
+	 for ( let i of list) {
+		 matriz[i][4] = new Block(colors.black);
+	 }
+	 matriz[5][4].walls[side.north] = wallState.portal;
+	 matriz[4][4].walls[side.south] = wallState.portal;
+	 matriz[0][4].event = BlockEvent.endOfLevel;
+	 matriz[0][4].color =colors.endOfLevel;
+	}
+	
